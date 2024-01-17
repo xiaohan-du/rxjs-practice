abed0ecc1225263fe810c069ebf6ab366435431c
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-17 20:35:12 +0000

    from: Creates an Observable from an Array, resolve Promise immediately

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,37 @@
-import { Observable, Observer, of } from 'rxjs';
-
-const observer: Observer<string> = {
-  next: (val: string): void => {
-    console.log(val);
-  },
-  error: (err: string): void => {
-    console.log(err)
-  },
-  complete: (): void => {
-    console.log("complete");
-  }
-}
+import { Observable, Observer, from, of } from "rxjs"
 
 const observable$ = new Observable<string>((subscriber) => {
-  try {
-    subscriber.next("Hi");
-  } catch {
-    subscriber.error("err");
-  };
+  subscriber.next("Hi");
+  subscriber.error("err");
   subscriber.complete();
 });
 
+const observer: Observer<unknown> = {
+  next: (val: unknown) => {
+    console.log(val);
+  },
+  error: (err: any) => {
+    console.log(err);
+  },
+  complete: () => {
+    console.log("complete");
+  }
+};
+
 observable$.subscribe(observer);
 
-of("a", "b", "c").subscribe(observer);
+of('a', 'b', 'c').subscribe(observer);
+
+from(['d', 'e', 'f']).subscribe(observer);
+
+
+const promise = new Promise((resolve) => {
+  resolve("Resolve");
+});
+
+// not showing resolved promise
+console.log(promise);
+
+const promiseObservable$ = from(promise);
+// showing resolved promise
+promiseObservable$.subscribe(observer);
